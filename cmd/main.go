728aa493d91e39f1d626a0afe2ace3a37ecdb016
KHaritmonds <kharitmonds@slb.com>
2025-04-30 19:41:04 +0530

    Theme from Financial Wisdom Forum

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"net/http"
+	"path/filepath"
+	"text/template"
 
 	"golang-bb/internal/logger"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	logger.Debugf(ctx, "GET /")
-	io.WriteString(w, "Welcome to Golang BB!")
+	httpMethod := r.Method
+	urlPath := filepath.Clean(r.URL.Path)
+	logger.Debugf(ctx, "%s %s", httpMethod, urlPath)
+
+	templateOutput, _ := template.ParseFiles("./view/templates/overall.html")
+	templateOutput.ExecuteTemplate(w, "layout", nil)
 }
 
 func main() {
 	ctx := context.Background()
-	http.HandleFunc("/", getRoot)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./view/static/assets/"))))
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("./view/static/styles/"))))
+	http.HandleFunc("/", serveTemplate)
 	portNumber := 9000
 	logger.Infof(ctx, "Server is listening on port %d", portNumber)
 	err := http.ListenAndServe(fmt.Sprintf("localhost:%d", portNumber), nil)
