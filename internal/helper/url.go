6f4784f962d03b02a16e2a61fee0625c59dbaa00
Kevin Haritmonds <haritmonds@yahoo.com>
2025-06-15 11:39:12 +0000

    Handle no SID case on URL generation

--- internal/helper/url.go
+++ internal/helper/url.go
@@ -2,10 +2,18 @@ package helper
 
 import "net/url"
 
+const (
+	NO_SID = ""
+)
+
 func UrlWithSID(rawUrl string, sessionId string) string {
 	u, _ := url.Parse(rawUrl)
 	q := u.Query()
-	q.Set("sid", sessionId)
+	if sessionId == NO_SID {
+		q.Del("sid")
+	} else {
+		q.Set("sid", sessionId)
+	}
 	u.RawQuery = q.Encode()
 	return u.String()
 }
--- test/internal/helper/url_test.go
+++ test/internal/helper/url_test.go
@@ -49,6 +49,49 @@ func TestUrlWithSID(t *testing.T) {
 	}
 }
 
+func TestUrlWithSID_NoSID(t *testing.T) {
+	{
+		actual := helper.UrlWithSID("./forums", helper.NO_SID)
+		expected := "./forums"
+		if actual != expected {
+			t.Errorf("Got %s, wanted %s", actual, expected)
+			return
+		}
+	}
+	{
+		actual := helper.UrlWithSID("./forums?f=1", helper.NO_SID)
+		expected := "./forums?f=1"
+		if actual != expected {
+			t.Errorf("Got %s, wanted %s", actual, expected)
+			return
+		}
+	}
+	{
+		actual := helper.UrlWithSID("./posts?p=101#p101", helper.NO_SID)
+		expected := "./posts?p=101#p101"
+		if actual != expected {
+			t.Errorf("Got %s, wanted %s", actual, expected)
+			return
+		}
+	}
+	{
+		actual := helper.UrlWithSID("./topics?f=1&start=100", helper.NO_SID)
+		expected := "./topics?f=1&start=100"
+		if actual != expected {
+			t.Errorf("Got %s, wanted %s", actual, expected)
+			return
+		}
+	}
+	{
+		actual := helper.UrlWithSID("./topics?f=1&sid=old&start=100", helper.NO_SID)
+		expected := "./topics?f=1&start=100"
+		if actual != expected {
+			t.Errorf("Got %s, wanted %s", actual, expected)
+			return
+		}
+	}
+}
+
 func TestUrlWithSID_Xss(t *testing.T) {
 	{
 		actual := helper.UrlWithSID("./top<script>ics?f=<script>1&start<script>=100", "123456")
@@ -61,6 +104,14 @@ func TestUrlWithSID_Xss(t *testing.T) {
 	{
 		actual := helper.UrlWithSID("./top%3Cscript%3Eics?f=%3Cscript%3E1&start%3Cscript%3E=100", "123456")
 		expected := "./top%3Cscript%3Eics?f=%3Cscript%3E1&sid=123456&start%3Cscript%3E=100"
+		if actual != expected {
+			t.Errorf("Got %s, wanted %s", actual, expected)
+			return
+		}
+	}
+	{
+		actual := helper.UrlWithSID("./top%3Cscript%3Eics?sid=old&f=%3Cscript%3E1&start%3Cscript%3E=100", helper.NO_SID)
+		expected := "./top%3Cscript%3Eics?f=%3Cscript%3E1&start%3Cscript%3E=100"
 		if actual != expected {
 			t.Errorf("Got %s, wanted %s", actual, expected)
 			return
